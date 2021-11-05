c4ac35dc6a922f531927b27435da20b09686c9bb
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-05 09:53:22 -0500

    Se adiciona pruebas al archivo de concatenarNombre.test

--- src/tests/domain/concatenarNombre.test.ts
+++ src/tests/domain/concatenarNombre.test.ts
@@ -4,15 +4,45 @@ import { concatenarNombre } from '../../domain/concatenarNombre';
 describe('Concatenar Nombre', () => {
     
     test('debe concatenar nombres y apellidos', () => {
-       const arrayValores = {
-           nombre1: 'Andres',
-           nombre2: 'Dario',
-           apellido1: 'Otalvaro',
-           apellido2: 'Sanchez' 
+       const arrayNombre = {
+           primerNombre: 'Andres',
+           segundoNombre: 'Dario',
+           primerApellido: 'Otalvaro',
+           segundoApellido: 'Sanchez' 
        };
 
-       expect(concatenarNombre(arrayValores)).toBe('Andres Dario Otalvaro Sanchez')
+       expect(concatenarNombre(arrayNombre)).toBe('Andres Dario Otalvaro Sanchez');
     });
     
+    test('debe concatenar primer nombre y primer apellido', () => {
+        const arrayNombre = {
+            primerNombre: 'Andres',
+            primerApellido: 'Otalvaro',
+        };
+        
+        expect(concatenarNombre(arrayNombre)).toBe('Andres Otalvaro');
+    });
+
+    test('No debe concatenar espacios en el nombre', () => {
+        const arrayNombre = {
+            primerNombre: 'Andrés ',
+            segundoNombre: ' Dario',
+            primerApellido: 'Otalvaro ',
+            segundoApellido: '    Sánchez' 
+        };
+
+        expect(concatenarNombre(arrayNombre)).toBe('Andrés Dario Otalvaro Sánchez');
+    });
     
+    test('debe concatenar nombre en mayusculas y minusculas', () => {
+        const arrayNombre = {
+            primerNombre: 'AnDrEs',
+            segundoNombre: 'DaRIo',
+            primerApellido: 'otalvarO',
+            segundoApellido: 'SanchEZ' 
+        };
+ 
+        expect(concatenarNombre(arrayNombre)).toBe('AnDrEs DaRIo otalvarO SanchEZ');
+    });
+       
 });
