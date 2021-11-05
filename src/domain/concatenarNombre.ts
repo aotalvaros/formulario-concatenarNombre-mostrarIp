b3b44eef5e219e2394fe7b3929914cadd874e261
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-05 09:56:56 -0500

    Se modifica el nombre de la condicion que esta validando el if en la funcion concatenarNombre

--- src/domain/concatenarNombre.ts
+++ src/domain/concatenarNombre.ts
@@ -3,7 +3,7 @@ import { INombre } from "../interface/INombre";
 export const concatenarNombre = (nombre: INombre): string => {
     let resultado: string
 
-    if (!(nombre.segundoApellido && nombre.segundoApellido)) {
+    if (!(nombre.segundoNombre && nombre.segundoApellido)) {
         resultado = `${nombre.primerNombre.trim()} ${nombre.primerApellido.trim()}`;
     } else {
         resultado = `${nombre.primerNombre.trim()} ${nombre.segundoNombre?.trim()} ${nombre.primerApellido.trim()} ${nombre.segundoApellido.trim()}`;
