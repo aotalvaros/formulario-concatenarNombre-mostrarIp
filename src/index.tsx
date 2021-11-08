df024e4ca278b97dc0383ed3c1ec5f6b9ece3ec8
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-08 10:26:50 -0500

    Se llama el componente Formulario desde el  index, para ser renderizado

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Formulario } from './components/Formulario';
 
 ReactDOM.render(
- <></>,
+ <Formulario/>,
   document.getElementById('root')
 );
-
-
