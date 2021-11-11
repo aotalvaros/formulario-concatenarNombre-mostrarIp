94cd7ded00037faeef4c9894132ca5737f73b39a
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-11 15:59:35 -0500

    Se adiciona mas pruebas en el archivo de calcularEdad.test

--- src/tests/domain/calcularEdad.test.ts
+++ src/tests/domain/calcularEdad.test.ts
@@ -1,19 +1,32 @@
 import { calcularEdad } from '../../domain/calcularEdad';
 
 describe('Calcular Edad', () => {
+    let fechaDeNacimiento: Date;
+    beforeAll(() => {
+       fechaDeNacimiento = new Date();
+    });
 
     test('debe calcular la edad por la fecha de nacimiento', () => {
-        const fechaNacimiento: Date = new Date();
-        fechaNacimiento.setFullYear(1990);
+        fechaDeNacimiento.setFullYear(1990);
 
-        expect(calcularEdad(fechaNacimiento).edad).toBe(31);
+        expect(calcularEdad(fechaDeNacimiento).edad).toBe(31);
+    });
+
+    test('debe calcular la edad por la fecha de nacimiento, segunda prueba', () => {
+        fechaDeNacimiento.setFullYear(2000);
+
+        expect(calcularEdad(fechaDeNacimiento).edad).toBe(21);
     });
 
     test('No debe recibir una fecha mayor a la actual', () => {
-        const fechaDeNacimiento: Date = new Date();
         fechaDeNacimiento.setFullYear(2250);
 
-        expect(calcularEdad(fechaDeNacimiento).error).toBe('Año invalido');
+        expect(calcularEdad(fechaDeNacimiento).error).toBe('Ups! ingresa una fecha que no sea mayor a la actual');
     });
 
+    test('No debe recibir una fecha mayor a la actual, segunda prueba', () => {
+        fechaDeNacimiento.setFullYear(2023);
+
+        expect(calcularEdad(fechaDeNacimiento).error).toBe('Ups! ingresa una fecha que no sea mayor a la actual');
+    });
 });
