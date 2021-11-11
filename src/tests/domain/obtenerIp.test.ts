154c8de3b39c8b5b7394bf9e7f21f38d280dab9b
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-10 22:07:06 -0500

    Se  crea un archivo para realizar pruebas en la funcion obtenerIp

---
+++ src/tests/domain/obtenerIp.test.ts
@@ -0,0 +1,12 @@
+import { obtenerIp } from '../../domain/obtenerIp';
+import { mockFunction } from '../../helpers/JestHelpers';
+
+jest.mock('../../domain/obtenerIp');
+
+describe('Obtener IP', () => {
+
+    test('debe devolver una direccio ip', async() => {     
+             
+    });
+    
+});
