f18593e6fe345069bbd7fd135002554d637943a4
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-11 16:00:25 -0500

    Se refactoriza el codigo de la funcion calcularEdad

--- src/domain/calcularEdad.ts
+++ src/domain/calcularEdad.ts
@@ -1,15 +1,14 @@
 import { IEdad } from "../interface/IEdad";
 
 export const calcularEdad = (fechaNacimiento: Date): IEdad => {
-    const fechaActual = new Date();
-    const anioActual: number = fechaActual.getFullYear();
-    let resultadoEdad: IEdad = {edad: 0, error: ''};
+   const anioActual: number = new Date().getFullYear(); 
+   let resultadoEdad: IEdad = { edad: 0, error: '' };
     
-    if ( fechaNacimiento.getFullYear() > anioActual) {
-       resultadoEdad.error = 'Año invalido';
-    } else {
-       resultadoEdad.edad = anioActual - fechaNacimiento.getFullYear();
-    };
+   if (fechaNacimiento.getFullYear() > anioActual) {
+      resultadoEdad.error = 'Ups! ingresa una fecha que no sea mayor a la actual';
+   } else {
+      resultadoEdad.edad = anioActual - fechaNacimiento.getFullYear();
+   };
     
-    return resultadoEdad;
+   return resultadoEdad;
 };
