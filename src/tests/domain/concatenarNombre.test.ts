23cbfcbe9e05ce5f12b7d088c69b57e345ac631b
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-11 16:13:22 -0500

    Se agrega pruebas al archivo concatenarNombre.test

--- src/tests/domain/concatenarNombre.test.ts
+++ src/tests/domain/concatenarNombre.test.ts
@@ -13,6 +13,17 @@ describe('Concatenar Nombre', () => {
 
        expect(concatenarNombre(arrayNombre)).toBe('Andres Dario Otalvaro Sanchez');
     });
+
+    test('debe concatenar nombres y apellidos, segunda prueba', () => {
+        const arrayNombre = {
+            primerNombre: "Pepito",
+            segundoNombre: "Pablo",
+            primerApellido: "Perez",
+            segundoApellido: "Pulgada" 
+        };
+ 
+        expect(concatenarNombre(arrayNombre)).toBe('Pepito Pablo Perez Pulgada');
+     });
     
     test('debe concatenar primer nombre y primer apellido', () => {
         const arrayNombre = {
@@ -23,15 +34,35 @@ describe('Concatenar Nombre', () => {
         expect(concatenarNombre(arrayNombre)).toBe('Andres Otalvaro');
     });
 
+    test('debe concatenar primer nombre y primer apellido, segunda prueba', () => {
+        const arrayNombre = {
+            primerNombre: 'Luis',
+            primerApellido: 'Quintero',
+        };
+        
+        expect(concatenarNombre(arrayNombre)).toBe('Luis Quintero');
+    });
+
     test('No debe concatenar espacios en el nombre', () => {
         const arrayNombre = {
-            primerNombre: 'Andrés ',
+            primerNombre: 'Andres ',
             segundoNombre: ' Dario',
             primerApellido: 'Otalvaro ',
-            segundoApellido: '    Sánchez' 
+            segundoApellido: '    Sanchez' 
         };
 
-        expect(concatenarNombre(arrayNombre)).toBe('Andrés Dario Otalvaro Sánchez');
+        expect(concatenarNombre(arrayNombre)).toBe('Andres Dario Otalvaro Sanchez');
+    });
+
+    test('No debe concatenar espacios en el nombre, segunda prueba', () => {
+        const arrayNombre = {
+            primerNombre: '  Pedro   ',
+            segundoNombre: '      Leon  ',
+            primerApellido: '  Restrepo  ',
+            segundoApellido: ' Perez   ' 
+        };
+
+        expect(concatenarNombre(arrayNombre)).toBe('Pedro Leon Restrepo Perez');
     });
     
     test('debe concatenar nombre en mayusculas y minusculas', () => {
@@ -43,6 +74,17 @@ describe('Concatenar Nombre', () => {
         };
  
         expect(concatenarNombre(arrayNombre)).toBe('AnDrEs DaRIo otalvarO SanchEZ');
+    });
+
+    test('debe concatenar nombre en mayusculas y minusculas, segunda prueba', () => {
+        const arrayNombre = {
+            primerNombre: 'pePitO',
+            segundoNombre: 'pABLo',
+            primerApellido: 'PeReZ',
+            segundoApellido: 'SanchEZ' 
+        };
+ 
+        expect(concatenarNombre(arrayNombre)).toBe('pePitO pABLo PeReZ SanchEZ');
     });
        
 });
