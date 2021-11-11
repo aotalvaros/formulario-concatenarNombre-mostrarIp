058cdc3e2dd8a3cafcdedc14c03a497323dd158e
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-10 23:03:43 -0500

    Se modifica la prueba para la funcion obtenerIp

--- src/tests/domain/obtenerIp.test.ts
+++ src/tests/domain/obtenerIp.test.ts
@@ -5,8 +5,13 @@ jest.mock('../../domain/obtenerIp');
 
 describe('Obtener IP', () => {
 
-    test('debe devolver una direccio ip', async() => {     
-             
+    const obtenerIpMock = mockFunction(obtenerIp);
+
+    test('debe devolver la direccion ip', async() => {     
+        const respuesta = '10.0.0.0'
+        obtenerIpMock.mockResolvedValue(respuesta);
+
+       await obtenerIp().then(data => expect(data).toEqual('10.0.0.0'));
     });
     
 });
