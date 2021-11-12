80b8ae171372616ac05d0653db38e16d38cd701d
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-12 08:13:33 -0500

    Se refactoriza el codigo de la prueba Formulario.test

--- src/tests/components/Formulario.test.tsx
+++ src/tests/components/Formulario.test.tsx
@@ -22,6 +22,19 @@ describe('Debe crear el formulario de registro', () => {
     let sweetAlertMock: any;
     let obtenerIpMock: any;
 
+    const primerNombre: string = "Andres";
+    const segundoNombre: string = "Dario";
+    const primerApellido: string = "Otalvaro";
+    const segundoApellido: string =  "Sanchez";
+    const anioNacimiento = '1988';
+    const nombre: INombre = {
+        primerNombre: primerNombre,
+        segundoNombre: segundoNombre, 
+        primerApellido: primerApellido,
+        segundoApellido: segundoApellido
+    };
+    let mensaje: string;
+
     beforeAll(() => {
         wrapper = shallow(<Formulario/>);
         concatenarNombreMock = mockFunction(concatenarNombre);
@@ -30,20 +43,8 @@ describe('Debe crear el formulario de registro', () => {
         obtenerIpMock = mockFunction(obtenerIp);
     });
 
-    test('debe mostrar una alerta de saludo cuando se ingrese todos los campos', async() => {      
-        const mensaje: string = 'Hola Andres Dario Otalvaro Sanchez, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices 33 años !';
-        const resultadoCalcularEdad: IEdad = { edad: 33, error: ''};
-        const primerNombre: string = "Andres";
-        const segundoNombre: string = "Dario";
-        const primerApellido: string = "Otalvaro";
-        const segundoApellido: string =  "Sanchez";
-        const nombre: INombre = {
-            primerNombre: primerNombre,
-            segundoNombre: segundoNombre, 
-            primerApellido: primerApellido,
-            segundoApellido: segundoApellido
-        };
-        const anioNacimiento = '1988';
+    beforeEach(() => {
+        mensaje = 'Hola Andres Dario Otalvaro Sanchez, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices 33 años !';
         wrapper.find("#formControlPrimerNombre").simulate('change', { target: { value: primerNombre, name: 'primerNombre' } });
         wrapper.find("#formControlSegundoNombre").simulate('change', { target: { value: segundoNombre, name: 'segundoNombre' } });
         wrapper.find("#formControlPrimerApellido").simulate('change', { target: { value: primerApellido, name: 'primerApellido' } });
@@ -51,7 +52,11 @@ describe('Debe crear el formulario de registro', () => {
         wrapper.find("#formControlCorreo").simulate('change', { target: { value: 'andres@gmail.com'} });
         wrapper.find("#formControlTelefono").simulate('change', { target: { value: '123456' } });
         wrapper.find("#datePickerFechaNacimiento").simulate('change',anioNacimiento);
+    })
 
+    test('debe mostrar una alerta de saludo cuando se ingrese todos los campos', async() => {      
+        const resultadoCalcularEdad: IEdad = { edad: 33, error: ''};
+  
         concatenarNombreMock.mockReturnValue('Andres Dario Otalvaro Sanchez');
         calcularEdadMock.mockReturnValue(resultadoCalcularEdad);
         sweetAlertMock.mockResolvedValue({
@@ -78,26 +83,8 @@ describe('Debe crear el formulario de registro', () => {
     });
 
     test('debe mostrar una alerta de saludo cuando se ingrese todos los campos, segunda prueba', async() => {      
-        const mensaje: string = 'Hola Pepito Pablo Perez Pulgada, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices 20 años !';
+        mensaje = 'Hola Pepito Pablo Perez Pulgada, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices 20 años !';
         const resultadoCalcularEdad: IEdad = { edad: 20, error: ''};
-        const primerNombre: string =   "Pepito";
-        const segundoNombre: string =  "Pablo";
-        const primerApellido: string =  "Perez";
-        const segundoApellido: string =  "Pulgada";
-        const nombre: INombre = {
-            primerNombre: primerNombre,
-            segundoNombre: segundoNombre, 
-            primerApellido: primerApellido,
-            segundoApellido: segundoApellido
-        };
-        const anioNacimiento = '1988';
-        wrapper.find("#formControlPrimerNombre").simulate('change', { target: { value: primerNombre, name: 'primerNombre' } });
-        wrapper.find("#formControlSegundoNombre").simulate('change', { target: { value: segundoNombre, name: 'segundoNombre' } });
-        wrapper.find("#formControlPrimerApellido").simulate('change', { target: { value: primerApellido, name: 'primerApellido' } });
-        wrapper.find("#formControlSegundoApellido").simulate('change', { target: { value: segundoApellido, name: 'segundoApellido' } });
-        wrapper.find("#formControlCorreo").simulate('change', { target: { value: 'andres@gmail.com'} });
-        wrapper.find("#formControlTelefono").simulate('change', { target: { value: '123456' } });
-        wrapper.find("#datePickerFechaNacimiento").simulate('change',anioNacimiento);
 
         concatenarNombreMock.mockReturnValue('Pepito Pablo Perez Pulgada');
         calcularEdadMock.mockReturnValue(resultadoCalcularEdad);
@@ -125,26 +112,7 @@ describe('Debe crear el formulario de registro', () => {
     });
 
     test('debe mostrar una alerta con la ip, luego de mostrar la alerta de saludo', async() => {
-        const mensaje: string = 'Hola Andres Dario Otalvaro Sanchez, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices 33 años !';
         const resultadoCalcularEdad: IEdad = { edad: 33, error: ''};
-        const primerNombre: string = "Andres";
-        const segundoNombre: string = "Dario";
-        const primerApellido: string = "Otalvaro";
-        const segundoApellido: string =  "Sanchez";
-        const nombre: INombre = {
-            primerNombre: primerNombre,
-            segundoNombre: segundoNombre, 
-            primerApellido: primerApellido,
-            segundoApellido: segundoApellido
-        };
-        const anioNacimiento = '1988';
-        wrapper.find("#formControlPrimerNombre").simulate('change', { target: { value: primerNombre, name: 'primerNombre' } });
-        wrapper.find("#formControlSegundoNombre").simulate('change', { target: { value: segundoNombre, name: 'segundoNombre' } });
-        wrapper.find("#formControlPrimerApellido").simulate('change', { target: { value: primerApellido, name: 'primerApellido' } });
-        wrapper.find("#formControlSegundoApellido").simulate('change', { target: { value: segundoApellido, name: 'segundoApellido' } });
-        wrapper.find("#formControlCorreo").simulate('change', { target: { value: 'andres@gmail.com'} });
-        wrapper.find("#formControlTelefono").simulate('change', { target: { value: '123456' } });
-        wrapper.find("#datePickerFechaNacimiento").simulate('change',anioNacimiento);
 
         concatenarNombreMock.mockReturnValue('Andres Dario Otalvaro Sanchez');
         calcularEdadMock.mockReturnValue(resultadoCalcularEdad);
@@ -171,23 +139,23 @@ describe('Debe crear el formulario de registro', () => {
     });
 
     test('debe mostrar una alerta con la ip, luego de mostrar la alerta de saludo, segunda prueba', async() => {
-        const mensaje: string = 'Hola Luis Felipe Quintero Vergara, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices 40 años !';
+        mensaje = 'Hola Luis Felipe Quintero Vergara, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices 40 años !';
         const resultadoCalcularEdad: IEdad = { edad: 40, error: ''};
-        const primerNombre: string = "Luis";
-        const segundoNombre: string = "Felipe";
-        const primerApellido: string = "Quintero";
-        const segundoApellido: string =  "Vergara";
-        const nombre: INombre = {
-            primerNombre: primerNombre,
-            segundoNombre: segundoNombre, 
-            primerApellido: primerApellido,
-            segundoApellido: segundoApellido
+        const primerNombreDos: string = "Luis";
+        const segundoNombreDos: string = "Felipe";
+        const primerApellidoDos: string = "Quintero";
+        const segundoApellidoDos: string =  "Vergara";
+        const nombreDos: INombre = {
+            primerNombre: primerNombreDos,
+            segundoNombre: segundoNombreDos, 
+            primerApellido: primerApellidoDos,
+            segundoApellido: segundoApellidoDos
         };
-        const anioNacimiento = '1988';
-        wrapper.find("#formControlPrimerNombre").simulate('change', { target: { value: primerNombre, name: 'primerNombre' } });
-        wrapper.find("#formControlSegundoNombre").simulate('change', { target: { value: segundoNombre, name: 'segundoNombre' } });
-        wrapper.find("#formControlPrimerApellido").simulate('change', { target: { value: primerApellido, name: 'primerApellido' } });
-        wrapper.find("#formControlSegundoApellido").simulate('change', { target: { value: segundoApellido, name: 'segundoApellido' } });
+        
+        wrapper.find("#formControlPrimerNombre").simulate('change', { target: { value: primerNombreDos, name: 'primerNombre' } });
+        wrapper.find("#formControlSegundoNombre").simulate('change', { target: { value: segundoNombreDos, name: 'segundoNombre' } });
+        wrapper.find("#formControlPrimerApellido").simulate('change', { target: { value: primerApellidoDos, name: 'primerApellido' } });
+        wrapper.find("#formControlSegundoApellido").simulate('change', { target: { value: segundoApellidoDos, name: 'segundoApellido' } });
         wrapper.find("#formControlCorreo").simulate('change', { target: { value: 'andres@gmail.com'} });
         wrapper.find("#formControlTelefono").simulate('change', { target: { value: '123456' } });
         wrapper.find("#datePickerFechaNacimiento").simulate('change',anioNacimiento);
@@ -209,7 +177,7 @@ describe('Debe crear el formulario de registro', () => {
             }
         });   
 
-        expect(concatenarNombreMock).toHaveBeenCalledWith(nombre);
+        expect(concatenarNombreMock).toHaveBeenCalledWith(nombreDos);
         expect(calcularEdadMock).toHaveBeenCalledWith(anioNacimiento);
         await expect(sweetAlertMock).toHaveBeenCalledWith(mensaje);
         await expect(obtenerIpMock).toHaveBeenCalled();
@@ -219,12 +187,6 @@ describe('Debe crear el formulario de registro', () => {
     test('debe mostrar una alerta de error cuando se ingresa una fecha mayor a la actual', () => {      
         const resultadoCalcularEdad: IEdad = { edad: 0, error: 'Ups! ingresa una fecha que no sea mayor a la actual'};
         const anioNacimiento = '2088';
-        wrapper.find("#formControlPrimerNombre").simulate('change', { target: { value: "Andres", name: 'primerNombre' } });
-        wrapper.find("#formControlSegundoNombre").simulate('change', { target: { value: "Dario", name: 'segundoNombre' } });
-        wrapper.find("#formControlPrimerApellido").simulate('change', { target: { value: "Otalvaro", name: 'primerApellido' } });
-        wrapper.find("#formControlSegundoApellido").simulate('change', { target: { value: "Sanchez", name: 'segundoApellido' } });
-        wrapper.find("#formControlCorreo").simulate('change', { target: { value: 'andres@gmail.com'} });
-        wrapper.find("#formControlTelefono").simulate('change', { target: { value: '123456' } });
         wrapper.find("#datePickerFechaNacimiento").simulate('change',anioNacimiento);
 
         calcularEdadMock.mockReturnValue(resultadoCalcularEdad);
@@ -240,8 +202,18 @@ describe('Debe crear el formulario de registro', () => {
         expect(sweetAlertMock).toHaveBeenCalledWith({icon: 'error',
             title: 'Ups! ingresa una fecha que no sea mayor a la actual'});
     });
+   
+    test('debe tener el campo de la fecha lleno por defecto con la fecha actual', () => {
+        const anioNacimiento = '01/01/2000';
+        wrapper.find("#datePickerFechaNacimiento").simulate('change',anioNacimiento);
+        expect(wrapper.find("#datePickerFechaNacimiento").prop('selected')).toBe(anioNacimiento)        
+    });
+    
+    test('debe validar que campo de la fecha siempre este lleno con la fecha actual', () => {
+        expect(wrapper.find("#datePickerFechaNacimiento").prop('selected')).toBe('1988') 
+    });
 
-    test('No debe mostrar la alerta cuando el formulario no sea valido', () => {      
+    test('No debe mostrar la alerta cuando el formulario sea valido', () => {      
         wrapper.find('#formulario').simulate('submit', {
             preventDefault: () => {},
             stopPropagation: () => {},
@@ -252,11 +224,37 @@ describe('Debe crear el formulario de registro', () => {
   
         expect(sweetAlertMock).not.toHaveBeenCalled();
     });
-
-    test('debe tener el campo de la fecha lleno por defecto con la fecha actual', () => {
-        const anioNacimiento = '01/01/2000';
-        wrapper.find("#datePickerFechaNacimiento").simulate('change',anioNacimiento);
-        expect(wrapper.find("#datePickerFechaNacimiento").prop('selected')).toBe(anioNacimiento)        
+    
+    test('No debe seguir validando el formulario cuando este en false', () => {
+        wrapper.find('#formulario').simulate('submit', {
+            preventDefault: () => {},
+            stopPropagation: () => {},
+            currentTarget: {
+                checkValidity: jest.fn().mockReturnValue(false)            
+            }
+        });
+        
+        expect(wrapper.find('#formulario').prop('noValidate')).toBe(true);
     });
-
+    
+    test('No debe mostrar el segundo alert con la ip si el boton de ok no es valido', () => {
+        const resultadoCalcularEdad: IEdad = { edad: 33, error: ''};
+  
+        concatenarNombreMock.mockReturnValue('Andres Dario Otalvaro Sanchez');
+        calcularEdadMock.mockReturnValue(resultadoCalcularEdad);
+        sweetAlertMock.mockResolvedValue({
+            isConfirmed: false,
+        }); 
+        wrapper.find('#formulario').simulate('submit', {
+            preventDefault: () => {},
+            stopPropagation: () => {},
+            currentTarget: {
+                checkValidity: jest.fn().mockReturnValue(true)            
+            }
+        });
+        expect(sweetAlertMock).toHaveBeenCalledWith(mensaje);
+        expect(sweetAlertMock).toBeCalledTimes(1);
+    });
+    
+    
 });
