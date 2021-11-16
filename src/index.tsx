b64fa6964332aa4330ff275d52c22af05a4df3d5
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-16 10:00:52 -0500

    Se importa los estilos al archivo index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style/index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Formulario } from './components/Formulario';
 
 ReactDOM.render(
