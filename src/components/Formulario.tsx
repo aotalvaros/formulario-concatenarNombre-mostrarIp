fe18a8130bc847ef909154fd91e84197633fdfab
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-17 07:45:53 -0500

    Se mejora el estilo del formulario y se crea una funcion para validar que el correo ingresado sea correcto

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
 import Swal, { SweetAlertResult } from 'sweetalert2';
 import { concatenarNombre } from '../domain/concatenarNombre';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,6 +9,7 @@ import DatePicker from "react-datepicker";
 import { calcularEdad } from '../domain/calcularEdad';
 import { IEdad } from '../interface/IEdad';
 import { obtenerIp } from '../domain/obtenerIp';
+import logoGobanUnidos from '../image/LogoInsti.png'
 
 export const Formulario = () => {
 
@@ -16,18 +17,24 @@ export const Formulario = () => {
         primerNombre: "",
         segundoNombre: "",
         primerApellido: "",
-        segundoApellido: "",     
+        segundoApellido: ""   
     });
     const [startDate, setStartDate] = useState<Date>(new Date());
     const [validated, setValidated] = useState<boolean>(false);
+    const [isOnlyTheEmailValid, setIsOnlyTheEmailValid] = useState<boolean>(false);
+    const [correo, setCorreo] = useState('');
     
     const handleSubmitForm = (event: any) => {                               
         const form = event.currentTarget;
         event.preventDefault();
         event.stopPropagation();                  
         setValidated(true);
-                                                
-        if (form.checkValidity()) {                    
+        setIsOnlyTheEmailValid(false);
+
+        if(event.currentTarget.Correo.checkValidity() && !isValidEmail(correo)){
+            setIsOnlyTheEmailValid(true);
+            setValidated(false); //---
+        } else if (form.checkValidity()) {                
             saludar(formulario); 
         };
     };
@@ -42,17 +49,36 @@ export const Formulario = () => {
         };
     };
 
+    const handleOnChangeCorreo = (event: any) =>{
+        setCorreo( event.target.value)
+    };
+
+    const isValidEmail = (mail: any) =>{
+        return /^\w+([\.\+\-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(mail);
+    };
+
     const saludar = (nombre: INombre)  => {          
         const nombreConcatenado = concatenarNombre(nombre);
         const {edad,error}: IEdad = calcularEdad(startDate);        
         if(error){
             mostrarErrorDeFechaMayorActual(error);
         } else if(!error) {
-            Swal.fire(`Hola ${nombreConcatenado}, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices ${edad} años !`)
+            Swal.fire({
+                title:`Hola ${nombreConcatenado}, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices ${edad} años !`,
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false
+            })
             .then((result: SweetAlertResult) => {
                 if (result.isConfirmed) {  
                     obtenerIp().then((ip) =>{ 
-                        Swal.fire(`Tu direccion ip es : ${ip}`, '', 'success');
+                        Swal.fire({
+                            title:`Tu direccion ip es : ${ip}`,
+                            icon:'success',
+                            allowOutsideClick: false,
+                            allowEscapeKey: false,
+                            allowEnterKey: false
+                        });
                     });                                           
                 }; 
             }); 
@@ -67,103 +93,147 @@ export const Formulario = () => {
     };
 
     return (  
-       <> 
-        <Form noValidate validated={validated} onSubmit={handleSubmitForm} id='formulario'>
-            <Row className="mb-3">
-             <Form.Group as={Col} md="4" >
-                <Form.Control
-                    required
-                    id="formControlPrimerNombre"
-                    name="primerNombre"
-                    type="text"
-                    placeholder="Primer Nombre"
-                    onChange={handleOnChange}                  
-                />
-                <Form.Control.Feedback type="invalid">
-                    Por favor ingrese el nombre.
-                </Form.Control.Feedback>
-             </Form.Group>
-             <Form.Group as={Col} md="4">
-                <Form.Control
-                    id='formControlSegundoNombre'
-                    name='segundoNombre'
-                    type="text"
-                    placeholder="Segundo Nombre" 
-                    onChange={handleOnChange}                   
-                />
-             </Form.Group>
-            </Row>
+       <>
+        <Container className='abs-center' fluid="md">  
+            <Form 
+                noValidate 
+                validated={validated} 
+                onSubmit={handleSubmitForm} 
+                id='formulario'
+                className='card card-container'
+            >
+            <img
+                className="img-serponsive logo-img" alt=""
+                src={logoGobanUnidos}
+            />                     
+            <Row className="mb-2">
+                <Col xs="12" lg="6" className='mb-2' >
+                    <Form.Group>
+                        <Form.Control                        
+                            required
+                            id="formControlPrimerNombre"
+                            name="primerNombre"
+                            type="text"
+                            placeholder="Primer Nombre"
+                            onChange={handleOnChange}
+                               
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Por favor ingrese el nombre.
+                        </Form.Control.Feedback>
+                    </Form.Group> 
+                </Col>
+                <Col xs="12" lg="6" className='mb-2'>
+                    <Form.Group >
+                        <Form.Control
+                            id='formControlSegundoNombre'
+                            name='segundoNombre'
+                            type="text"
+                            placeholder="Segundo Nombre" 
+                            onChange={handleOnChange}
+                                          
+                        />
+                    </Form.Group> 
+                </Col>          
+                </Row>
 
-            <Row className="mb-3">
-             <Form.Group as={Col} md="4" >
-                <Form.Control
-                    required
-                    id="formControlPrimerApellido"
-                    name="primerApellido"
-                    type="text"
-                    placeholder="Primer apellido"
-                    onChange={handleOnChange} 
-                />
-                <Form.Control.Feedback type="invalid">
-                    Por favor ingrese el apellido.
-                </Form.Control.Feedback>
-             </Form.Group>
-             <Form.Group as={Col} md="4" >
-                <Form.Control
-                    id="formControlSegundoApellido"
-                    name="segundoApellido"
-                    type="text"
-                    placeholder="Segundo apellido"
-                    onChange={handleOnChange}
-                />
-             </Form.Group>
-            </Row>
-            <Row>
-            <Form.Group as={Col} md="4" >
-                <DatePicker 
-                    id='datePickerFechaNacimiento'
-                    placeholderText='Fecha cumpleaños'
-                    selected={startDate}
-                    onChange={onChangeDatePicker}
-                    showYearDropdown
-                    dateFormatCalendar="MMMM"
-                    yearDropdownItemNumber={15}
-                    scrollableYearDropdown
-                />            
-             </Form.Group>
-            </Row>
+                <Row className="mb-2">
+                    <Col xs="12" lg="6" className='mb-2'>
+                        <Form.Group>
+                            <Form.Control
+                                required
+                                id="formControlPrimerApellido"
+                                name="primerApellido"
+                                type="text"
+                                placeholder="Primer apellido"
+                                onChange={handleOnChange} 
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                Por favor ingrese el apellido.
+                            </Form.Control.Feedback>
+                        </Form.Group>                           
+                    </Col>
+                    <Col xs="12" lg="6" className='mb-2'>
+                        <Form.Group>    
+                            <Form.Control
+                                id="formControlSegundoApellido"
+                                name="segundoApellido"
+                                type="text"
+                                placeholder="Segundo apellido"
+                                onChange={handleOnChange}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                
+                <Row >
+                    <Col lg="4" className="mb-2">
+                        <InputGroup>
+                            <DatePicker  
+                                required                         
+                                id='datePickerFechaNacimiento'
+                                placeholderText='Fecha cumpleaños'
+                                selected={startDate}
+                                onChange={onChangeDatePicker}
+                                showYearDropdown
+                                dateFormatCalendar="MMMM"
+                                yearDropdownItemNumber={15}
+                                scrollableYearDropdown
+                                maxDate={new Date()}
+                            />            
+                        </InputGroup>
+                    </Col>
+                </Row>
 
-            <Row>
-             <Form.Group as={Col} md="4" >
-                <Form.Control
-                    required
-                    id="formControlCorreo"
-                    type="text"
-                    placeholder="Correo"
-                />
-                <Form.Control.Feedback type="invalid">
-                    Por favor ingrese el correo.
-                </Form.Control.Feedback>
-             </Form.Group>
-             <Form.Group as={Col} md="4" >
-                <Form.Control
-                    required
-                    id="formControlTelefono"
-                    type="text"
-                    placeholder="Telefono"
-                />
-                <Form.Control.Feedback type="invalid">
-                    Por favor ingrese el telefono.
-                </Form.Control.Feedback>
-             </Form.Group>
-            </Row>
-            <Button 
-                type='submit'
-                id='buttonMostrarMensaje'
-               >
-                Saludo
-            </Button>
-        </Form>
+                <Row className="mb-2">
+                    <InputGroup >
+                        <Form.Control   
+                            required              
+                            id="formControlCorreo"
+                            type="text"
+                            placeholder="Correo"
+                            name='Correo'
+                            onChange={handleOnChangeCorreo}
+                            isInvalid={isOnlyTheEmailValid}                           
+                        />  
+                        {
+                            !isOnlyTheEmailValid ? (
+                                <Form.Control.Feedback type="invalid">
+                                    Por favor ingrese el correo.
+                                </Form.Control.Feedback>
+                            ) : (
+                                <Form.Control.Feedback type="invalid">
+                                    Por favor ingrese un correo valido como: 'nombre@correo.com'
+                                </Form.Control.Feedback>                            
+                            )
+                        }
+                    </InputGroup>                
+                </Row>
+
+                <Row>
+                    <Col lg="12">
+                        <Form.Group>
+                            <Form.Control
+                                required
+                                id="formControlTelefono"
+                                type="number"
+                                placeholder="Telefono"
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                Por favor ingrese el telefono.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Button 
+                    type='submit'
+                    id='buttonMostrarMensaje'
+                    className="text-center mb-2"
+                >
+                    Saludo
+                </Button>
+            </Form>
+        </Container> 
      </>        
     );
 }
