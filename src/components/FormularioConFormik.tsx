7ca63c0a17bdfaddebd48e05f221d348f8a4a5b7
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-23 15:38:15 -0500

    Se agrega estilo a los componentes del FormularioConFormik

--- src/components/FormularioConFormik.tsx
+++ src/components/FormularioConFormik.tsx
@@ -1,57 +1,68 @@
-import { Field, Formik, Form, ErrorMessage } from "formik";
-import React from "react";
-import * as Yup from "yup";
-import Swal, { SweetAlertResult } from "sweetalert2";
-import { INombre } from "../interface/INombre";
-import { concatenarNombre } from "../domain/concatenarNombre";
-import { calcularEdad } from "../domain/calcularEdad";
-import { IEdad } from "../interface/IEdad";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";  
+import { calcularEdad } from "../domain/calcularEdad";
+import { concatenarNombre } from "../domain/concatenarNombre";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Field, Formik, Form, ErrorMessage } from "formik";
 import { FormDatePicker } from "./utils/custom-formik/FormikDatePicker";
+import { IEdad } from "../interface/IEdad";
+import { INombre } from "../interface/INombre";
 import { obtenerIp } from "../domain/obtenerIp";
+import * as Yup from "yup";
+import React from "react";
+import Swal, { SweetAlertResult } from "sweetalert2";
+import logoGobanUnidos from '../image/LogoInsti.png'
 
 export const FormularioConFormik = () => {
 
 
   const handleOnChange = (evento: any) => {
-
     const nombre: INombre = {
       primerNombre: evento.inputPrimerNombre,
       primerApellido: evento.inputPrimerApellido,
       segundoNombre: evento.inputSegundoNombre,
       segundoApellido: evento.inputSegundoApellido
-    };
-    
+    };    
     saludar(nombre, evento.inputFechaNacimiento);
   };
   
   const saludar = (nombre: INombre, fechaNacimiento: Date) => {
     const nombreConcatenado = concatenarNombre(nombre);
     const { edad, error }: IEdad = calcularEdad(fechaNacimiento);
-    
+
+    if (error) {
+      mostrarErrorDeFechaMayorActual(error)
+    } else {
+      Swal.fire({
+        title: `Hola ${nombreConcatenado}, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices ${edad} años !`,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false
+      }).then((result: SweetAlertResult) => {
+        if (result.isConfirmed) {  
+          obtenerIp().then((ip) =>          
+            Swal.fire({
+              title:`Tu direccion ip es : ${ip}`,
+              icon:'success',
+              allowOutsideClick: false,
+              allowEscapeKey: false,
+              allowEnterKey: false
+            })
+          );                                        
+        };
+      });   
+    }  
+  };
+
+  const mostrarErrorDeFechaMayorActual = (error: string) => {   
     Swal.fire({
-      title: `Hola ${nombreConcatenado}, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices ${edad} años !`,
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-      allowEnterKey: false
-    }).then((result: SweetAlertResult) => {
-      if (result.isConfirmed) {  
-        obtenerIp().then((ip) =>          
-          Swal.fire({
-            title:`Tu direccion ip es : ${ip}`,
-            icon:'success',
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            allowEnterKey: false
-          })
-        );                                        
-      };
+      icon: 'error',
+      title: error,
     });
- 
   };
 
   return (
-    <div>
+    <>
       <Formik
         initialValues={{
           inputPrimerNombre: "",
@@ -74,66 +85,109 @@ export const FormularioConFormik = () => {
             .email("El correo no tiene un formato valido.")
             .required("Por favor ingrese el correo."),
           inputFechaNacimiento:Yup.date()
-            .nullable()
-            .required("Por favor ingrese una fecha."),
+            .required("Por favor ingrese una fecha.")
+            .max(new Date(), "Debes ingresar una fecha de nacimiento valida"),
           inputTelefono: Yup.string()
           .required("Por favor un ingrese numero de telefono."),
         })
         }
       >
         {(formik: any, props: any) => (
-          <Form>
-            <Field 
-              name="inputPrimerNombre"
-              type="text"
-              placeholder="Primer Nombre"
+        <Container className='abs-center' fluid="md"> 
+          <Form
+            className='card card-container'
+          >
+            <img
+                className="img-serponsive logo-img" alt=""
+                src={logoGobanUnidos}
             />
-            <ErrorMessage data-testid="errorPrimerNombre" name='inputPrimerNombre' component='span'/>
-            
-            <Field
-              name="inputSegundoNombre"
-              type="text"
-              placeholder="Segundo Nombre"
-            />
-            <Field
-              name="inputPrimerApellido"
-              type="text"
-              placeholder="Primer Apellido"
-            />
-            <ErrorMessage data-testid="errorPrimerApellido" name='inputPrimerApellido' component='span'/>
-            
-            <Field
-              name="inputSegundoApellido"
-              type="text"
-              placeholder="Segundo Apellido"
-            />
-            <FormDatePicker
-              showYearDropdown
-              dateFormatCalendar="MMMM"
-              yearDropdownItemNumber={50}
-              scrollableYearDropdown
-              maxDate={new Date()}
-            />
-            <ErrorMessage data-testid="errorFecha" name='inputFechaNacimiento' component='span'/>
+            <Row className="mb-3">
+              <Col xs="12" lg="6" >
+                <Field 
+                  name="inputPrimerNombre"
+                  type="text"
+                  placeholder="Primer Nombre"
+                  />
+              </Col>
+              <Col xs="12" lg="6">
+                <Field
+                  name="inputSegundoNombre"
+                  type="text"
+                  placeholder="Segundo Nombre"
+                  />
+              </Col>
+              <Col xs="12" lg="6">
+                <ErrorMessage data-testid="errorPrimerNombre" name='inputPrimerNombre' component='span'/>               
+              </Col>
+            </Row> 
 
-            <Field 
-              name="inputCorreo" 
-              type="text" 
-              placeholder="Correo" 
-            />
-            <ErrorMessage data-testid="errorCorreo" name='inputCorreo' component='span'/>
+            <Row className="mb-3">
+              <Col xs="12" lg="6">
+                <Field
+                  name="inputPrimerApellido"
+                  type="text"
+                  placeholder="Primer Apellido"
+                />
+              </Col>
+              <Col xs="12" lg="6">
+              <Field
+                name="inputSegundoApellido"
+                type="text"
+                placeholder="Segundo Apellido"
+                />
+              </Col>
+              <Col xs="12" lg="8">
+              <ErrorMessage data-testid="errorPrimerApellido" name='inputPrimerApellido' component='span'/>              
+              </Col>
+            </Row>
 
-            <Field 
-              name="inputTelefono" 
-              type="text" 
-              placeholder="Telefono" 
-            />
-            <ErrorMessage data-testid="errorTelefono" name='inputTelefono' component='span'/>
-            
-            <button type="submit">Registrar</button>
+            <Row className="mb-3">
+              <Col xs="12" lg="6">
+                <FormDatePicker
+                  showYearDropdown
+                  dateFormatCalendar="MMMM"
+                  yearDropdownItemNumber={50}
+                  scrollableYearDropdown
+                  maxDate={new Date()}
+                />
+              </Col>
+              <Col xs="12" lg="8">
+              <ErrorMessage data-testid="errorFecha" name='inputFechaNacimiento' component='span'/>         
+              </Col>
+            </Row>
+
+            <Row className="mb-3">
+              <Col xs="12" lg="6">
+                <Field 
+                  name="inputCorreo" 
+                  type="text" 
+                  placeholder="Correo" 
+                  />
+              </Col>
+              <Col xs="12" lg="6">
+                <Field 
+                  name="inputTelefono" 
+                  type="text" 
+                  placeholder="Telefono" 
+                  />
+              </Col>
+              <Col xs="12" lg="6">
+                <ErrorMessage data-testid="errorCorreo" name='inputCorreo' component='span'/>
+              </Col>
+              <Col xs="12" lg="6">
+                <ErrorMessage data-testid="errorTelefono" name='inputTelefono' component='span'/>
+              </Col>
+            </Row>
+            <Button 
+              type="submit"
+              className="text-center mb-2"
+            >
+              Registrar
+            </Button>
           </Form>
+        </Container>
         )}
       </Formik>
-    </div>
+    </>
   );
 };
