78353d7278032bd6978445a6153c85309ad7a188
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-25 10:22:11 -0500

    Se agrega etiquetas de estilos al fomularioConFormik

--- src/components/FormularioConFormik.tsx
+++ src/components/FormularioConFormik.tsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";  
 import { calcularEdad } from "../domain/calcularEdad";
 import { concatenarNombre } from "../domain/concatenarNombre";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, FormControl , Row } from "react-bootstrap";
 import { Field, Formik, Form, ErrorMessage } from "formik";
 import { FormDatePicker } from "./utils/custom-formik/FormikDatePicker";
 import { IEdad } from "../interface/IEdad";
@@ -11,7 +11,8 @@ import { obtenerIp } from "../domain/obtenerIp";
 import * as Yup from "yup";
 import React from "react";
 import Swal, { SweetAlertResult } from "sweetalert2";
-import logoGobanUnidos from '../image/LogoInsti.png'
+import logoGobanUnidos from '../image/LogoInsti.png';
+
 
 export const FormularioConFormik = () => {
 
@@ -92,89 +93,82 @@ export const FormularioConFormik = () => {
         })
         }
       >
-        {(formik: any, props: any) => (
-        <Container className='abs-center' fluid="md"> 
-          <Form
+        {({ handleSubmit, errors, touched }:any) => (
+
+        <Container className='abs-center' fluid="sm"> 
+          <Form 
+            noValidate
+            onSubmit={handleSubmit}
             className='card card-container'
           >
             <img
                 className="img-serponsive logo-img" alt=""
                 src={logoGobanUnidos}
             />
-            <Row className="mb-3">
-              <Col xs="12" lg="6" >
-                <Field 
-                  name="inputPrimerNombre"
-                  type="text"
-                  placeholder="Primer Nombre"
-                  />
+            <Row className="mb-2">
+              <Col xs="12" lg="6" className="mb-2">
+                <Field name="inputPrimerNombre">
+                  {({ field }: any) => (
+                    <FormControl {...field} type="text" placeholder="Primer Nombre"  isInvalid={!!errors.inputPrimerNombre && touched.inputPrimerNombre}/>
+                  )}
+                </Field>
+                <ErrorMessage data-testid="errorPrimerNombre" name='inputPrimerNombre' component='span'/> 
+
               </Col>
-              <Col xs="12" lg="6">
-                <Field
-                  name="inputSegundoNombre"
-                  type="text"
-                  placeholder="Segundo Nombre"
-                  />
-              </Col>
-              <Col xs="12" lg="6">
-                <ErrorMessage data-testid="errorPrimerNombre" name='inputPrimerNombre' component='span'/>               
-              </Col>
+
+              <Col xs="12" lg="6"  className="mb-2">
+                <Field name="inputSegundoNombre">
+                  {({ field }: any) => (
+                    <FormControl {...field} type="text" placeholder="Segundo Nombre"/>
+                  )}
+                </Field>
+              </Col>             
             </Row> 
 
             <Row className="mb-3">
-              <Col xs="12" lg="6">
-                <Field
-                  name="inputPrimerApellido"
-                  type="text"
-                  placeholder="Primer Apellido"
-                />
+              <Col xs="12" lg="6"  className="mb-2">
+                <Field name="inputPrimerApellido">
+                  {({ field }: any) => (
+                    <FormControl {...field} type="text" placeholder="Primer Apellido" isInvalid={!!errors.inputPrimerApellido && touched.inputPrimerApellido}/>
+                  )}
+                </Field>
+                <ErrorMessage data-testid="errorPrimerApellido" name='inputPrimerApellido' component='span'/>
               </Col>
-              <Col xs="12" lg="6">
-              <Field
-                name="inputSegundoApellido"
-                type="text"
-                placeholder="Segundo Apellido"
-                />
-              </Col>
-              <Col xs="12" lg="8">
-              <ErrorMessage data-testid="errorPrimerApellido" name='inputPrimerApellido' component='span'/>              
+
+              <Col xs="12" lg="6"  className="mb-2">
+              <Field name="inputSegundoApellido">
+                  {({ field }: any) => (
+                    <FormControl {...field} type="text" placeholder="Segundo Apellido" />
+                  )}
+              </Field>
               </Col>
             </Row>
 
             <Row className="mb-3">
-              <Col xs="12" lg="6">
-                <FormDatePicker
-                  showYearDropdown
-                  dateFormatCalendar="MMMM"
-                  yearDropdownItemNumber={50}
-                  scrollableYearDropdown
-                  maxDate={new Date()}
-                />
+              <Col xs="12" lg="6"  className="mb-2">
+                <FormDatePicker/>              
               </Col>
-              <Col xs="12" lg="8">
-              <ErrorMessage data-testid="errorFecha" name='inputFechaNacimiento' component='span'/>         
+              <Col xs="12">
+              <ErrorMessage data-testid="errorFecha" name='inputFechaNacimiento' component='span'/>       
               </Col>
             </Row>
 
             <Row className="mb-3">
-              <Col xs="12" lg="6">
-                <Field 
-                  name="inputCorreo" 
-                  type="text" 
-                  placeholder="Correo" 
-                  />
-              </Col>
-              <Col xs="12" lg="6">
-                <Field 
-                  name="inputTelefono" 
-                  type="text" 
-                  placeholder="Telefono" 
-                  />
-              </Col>
-              <Col xs="12" lg="6">
+              <Col xs="12" lg="6"  className="mb-2">
+                <Field name="inputCorreo">
+                  {({ field }: any) => (
+                    <FormControl {...field} type="text" placeholder="Correo" isInvalid={!!errors.inputCorreo && touched.inputCorreo}/>
+                  )}  
+                </Field>
                 <ErrorMessage data-testid="errorCorreo" name='inputCorreo' component='span'/>
               </Col>
-              <Col xs="12" lg="6">
+
+              <Col xs="12" lg="6"  className="mb-2">
+                <Field name="inputTelefono">
+                  {({ field }: any) => (
+                    <FormControl {...field} type="text" placeholder="Telefono" isInvalid={!!errors.inputTelefono && touched.inputTelefono}/>
+                  )}
+              </Field>
                 <ErrorMessage data-testid="errorTelefono" name='inputTelefono' component='span'/>
               </Col>
             </Row>
