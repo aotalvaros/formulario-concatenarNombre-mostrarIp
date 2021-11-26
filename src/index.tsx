2e49d820e57d9c243ab88f14105b4f8fea919b3b
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-26 16:47:28 -0500

    Se realiza modificaciones en el index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import './style/index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Formulario } from './components/Formulario';
+import { FormularioConFormik } from './components/FormularioConFormik';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import DateFnsUtils from "@date-io/date-fns";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 
-ReactDOM.render(
- <Formulario/>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <LocalizationProvider dateAdapter={DateFnsUtils}>
+      <FormularioConFormik />
+    </LocalizationProvider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
