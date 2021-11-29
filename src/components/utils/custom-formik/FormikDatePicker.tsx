edc2eaa93771855a48fdce42d0847ad5606b89fa
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-11-29 15:35:33 -0500

    Se modifica la clase de FormDatePicker donde se llama el  datePicker

--- src/components/utils/custom-formik/FormikDatePicker.tsx
+++ src/components/utils/custom-formik/FormikDatePicker.tsx
@@ -1,21 +1,15 @@
-import { Field } from "formik";
-import DatePicker from "react-datepicker";
+import { DatePicker } from "@material-ui/pickers";
 
 export const FormDatePicker = (props: any) => {
-  return (
-    <Field name="inputFechaNacimiento">
-      {({ field, meta, form: { setFieldValue } }: any) => {
-        return (
-          <DatePicker
-            {...props}
-            {...field}
-            selected={field.value || null}
-            onChange={(val) => {
-              setFieldValue(field.name, val);
-            }}
-          />
-        );
-      }}
-    </Field>
+  const {field, form} = props;
+  
+  return(      
+    <DatePicker
+      {...props}
+      {...field}                                                 
+      onChange={(val) => {                  
+        form.setFieldValue(field.name, val);                                                                  
+      }}      
+    />            
   );
 };
