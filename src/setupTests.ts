af43afbe2a8a25a82e89525eba28b496b8b45ec9
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-02 08:10:32 -0500

    se configura el archivo setupTests para el enzyme 17

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,13 +1,15 @@
 import '@testing-library/jest-dom';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+
 import {createSerializer} from 'enzyme-to-json';
 
 //@ts-ignore
 expect.addSnapshotSerializer(createSerializer({
     mode: 'deep',
     noKey: true
- }));
+}));
 
 Enzyme.configure({ adapter: new Adapter() });
 
