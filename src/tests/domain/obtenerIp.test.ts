53a689b72e66a19f5695e95086754d307d8dbd42
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-03 16:07:41 -0500

    Se adiciona pruebas al archivo  de obtenerIp

--- src/tests/domain/obtenerIp.test.ts
+++ src/tests/domain/obtenerIp.test.ts
@@ -5,17 +5,32 @@ jest.mock('../../domain/obtenerIp');
 
 describe('Obtener IP', () => {
 
-    const obtenerIpMock = mockFunction(obtenerIp);
+    let respuesta: any;
+    const obtenerIpMock: jest.MockedFunction<() => Promise<any>> = mockFunction(obtenerIp);
 
     test('debe devolver la direccion ip', async() => {     
-        const respuesta = '10.0.0.0'
+        respuesta = '10.0.0.0'
         obtenerIpMock.mockResolvedValue(respuesta);
 
        await obtenerIp().then(data => expect(data).toEqual('10.0.0.0'));
     });
+
+    test('debe devolver la direccion ip, segunda prueba', async() => {     
+        respuesta = '170.0.80.10'
+        obtenerIpMock.mockResolvedValue(respuesta);
+
+       await obtenerIp().then(data => expect(data).toEqual('170.0.80.10'));
+    });
+
+    test('debe devolver la direccion ip, tercera prueba', async() => {     
+        respuesta = '181.71.45.27'
+        obtenerIpMock.mockResolvedValue(respuesta);
+
+       await obtenerIp().then(data => expect(data).toEqual('181.71.45.27'));
+    });
     
     test('debe devolver un error', async() => {
-        const respuesta = 'Error de conexion';
+        respuesta = 'Error de conexion';
         obtenerIpMock.mockResolvedValue(respuesta);
     
        await obtenerIp().catch(error => {
