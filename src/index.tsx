39b7a553b0c5f824c887ba8a8f160f21f4f20641
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-03 16:10:00 -0500

    Se utiliza la etiqueta de MuiPickerUtilisProvider en el index

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import './style/index.css';
-import "bootstrap/dist/css/bootstrap.min.css";
 import { FormularioConFormik } from './components/FormularioConFormik';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import DateFnsUtils from "@date-io/date-fns";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 
 function App() {
   return (
-    <LocalizationProvider dateAdapter={DateFnsUtils}>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <FormularioConFormik />
-    </LocalizationProvider>
+    </MuiPickersUtilsProvider>
   );
 }
 
