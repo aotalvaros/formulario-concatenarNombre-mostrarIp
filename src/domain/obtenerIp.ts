1f8840bf12821850bd0925a262449f7e1be93b01
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-06 09:55:30 -0500

    Se refactoriza la funcion de obtenerIp

--- src/domain/obtenerIp.ts
+++ src/domain/obtenerIp.ts
@@ -3,11 +3,12 @@ export const obtenerIp = (): Promise<any> => {
     const axios = require('axios').default;
 
     return axios.get("https://api.ipify.org/?format=json")
-        .then(({data}: any) => data.ip)
+        .then((response: any) => response.data ? response.data.ip : "")
         .catch((error: any) => resolverError(error));                                
 };
 
-const resolverError = (error: any): string => { 
-    return 'Error de conexion';
+const resolverError = (error: any): Promise<any> => {
+    console.error(error);  
+    return Promise.reject('Error de conexion');   
 };
 
