47e440c93189da80b7a384cf1e98f9c6ecf2a5d5
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-09 16:57:44 -0500

    Se refactoriza y se tipa la funcion obtenerIP

--- src/domain/obtenerIp.ts
+++ src/domain/obtenerIp.ts
@@ -1,14 +1,15 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { IResponseIP } from '../interface/IResponseIP';
 
-export const obtenerIp = (): Promise<any> => {
-    const axios = require('axios').default;
+export const obtenerIp = (): Promise<string> => {
 
     return axios.get("https://api.ipify.org/?format=json")
-        .then((response: any) => response.data ? response.data.ip : "")
-        .catch((error: any) => resolverError(error));                                
+        .then((response: AxiosResponse<IResponseIP>) => response.data ? response.data.ip : "")
+        .catch(resolverError);                                
 };
 
-const resolverError = (error: any): Promise<any> => {
-    console.error(error);  
+const resolverError = (error: AxiosError): Promise<string> => {
+    console.log('Error consumiendo el servicio de obtener IP por => ', error);  
     return Promise.reject('Error de conexion');   
 };
 
