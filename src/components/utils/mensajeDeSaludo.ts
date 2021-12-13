cdf94c3a2e023e76f84afbe5302255813dd44e9f
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-13 11:03:18 -0500

    Se crea una clase llamada mensajeDeSaludo donde tendra la funcion saludar

---
+++ src/components/utils/mensajeDeSaludo.ts
@@ -0,0 +1,46 @@
+import Swal, { SweetAlertResult } from "sweetalert2";
+import { calcularEdad } from "../../domain/calcularEdad";
+import { concatenarNombre } from "../../domain/concatenarNombre";
+import { obtenerIp } from "../../domain/obtenerIp";
+import { IEdad } from "../../interface/IEdad";
+import { INombre } from "../../interface/INombre";
+
+export const saludar = (nombre: INombre, fechaNacimiento: Date, resetForm: any) => {
+    
+    const nombreConcatenado = concatenarNombre(nombre);
+    const { edad, error }: IEdad = calcularEdad(fechaNacimiento);
+
+    if (error) {
+        Swal.fire({
+            icon: "error",
+            title: error,
+          });
+    } else {
+      Swal.fire({
+        title: `Hola ${nombreConcatenado}, su registro fue exitoso, nos vemos en su cumpleaños. ¡Felices ${edad} años !`,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+      }).then((result: SweetAlertResult) => {
+        if (result.isConfirmed) {
+          obtenerIp().then((ip) => { 
+            Swal.fire({ 
+              title: `Tu direccion ip es : ${ip}`, 
+              icon: "success", 
+              allowOutsideClick: false, 
+              allowEscapeKey: false, 
+              allowEnterKey: false
+            });
+          }).catch((error) => {
+            Swal.fire({ 
+              title: error, 
+              icon: "error", 
+              allowOutsideClick: false, 
+              allowEscapeKey: false, 
+              allowEnterKey: false
+            });
+          }).finally(() => resetForm());         
+        }
+      });
+    }
+  };
